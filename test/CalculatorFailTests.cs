9ba7f6062ef36453e5511a9279cb0bda4f096e5a
DavidHernandezMorones <david.2.hernandez@atos.net>
2022-07-20 12:18:03 -0500

    Comprobar la excepcion al dividir entre cero

---
+++ test/CalculatorFailTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Calculators;
+using Xunit;
+
+namespace Test {
+	public class CalculatorFailTests {
+		[Fact]
+		public void Div_Should_RaiseError_WhenDividedByZero() {
+			// Arrange
+			var calc = new Calculator();
+			float a = 5;
+			float b = 0;
+
+			// Act
+			// Assert
+			Assert.Throws<DivideByZeroException>(() => calc.Div(a, b));
+		}
+	}
+}
