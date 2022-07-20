5a85ced4c11f24b8a78c0b8924cec990ac60b470
DavidHernandezMorones <david.2.hernandez@atos.net>
2022-07-20 12:18:37 -0500

    Usar Mock para verificar la dependecia de ICalculator sobre Factorial y Fibonacci

---
+++ test/SeriesMockTests.cs
@@ -0,0 +1,51 @@
+﻿using Calculators;
+using Moq;
+using Xunit;
+
+namespace Test {
+	public class SeriesMockTests {
+		[Fact]
+		public void Fibonacci_Should_ReturnNumber_OfSucession() {
+			// Arrange
+			int expected = 21;
+			int indexSucesion = 8;
+			
+			var mock = new Mock<ICalculator>();
+
+			mock.Setup(calc =>
+							calc.Add(It.IsAny<int>(), It.IsAny<int>())
+					)
+			.Returns((int a, int b) => a + b);
+
+			Series myTestSeries = new Series(mock.Object);
+
+			// Act
+			int result = myTestSeries.Fibonacci(indexSucesion);
+
+			//Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Factorial_Should_ReturnFactorial_OfNumber() {
+			// Arrange
+			int excpeted = 720;
+			int factorial = 6;
+
+			var mock = new Mock<ICalculator>();
+
+			mock.Setup(calc =>
+							calc.Mul(It.IsAny<int>(), It.IsAny<int>())
+					)
+			.Returns((int a, int b) => a * b);
+
+			Series myTestSeries = new Series(mock.Object);
+
+			//Act
+			int result = myTestSeries.Factorial(factorial);
+
+			//Assert
+			Assert.Equal(excpeted, result);
+		}
+	}
+}
